0ac83c0d1e256bbbcd4270f7e9b3d64ddf11b665
Cs Fahad <csfahadofficiall@gmail.com>
2023-02-27 20:49:09 +0530

    configuration_updated✨

--- next.config.js
+++ next.config.js
@@ -3,4 +3,8 @@ module.exports = {
   images: {
     domains: ["links.papareact.com"],
   },
+  env: {
+    MapboxAccessToken:
+      "pk.eyJ1IjoiY3NmYWhhZCIsImEiOiJjbGVrNmpnMW8wODMwM29vZGhmbzF3d2t3In0.5yFH0e6qllFHZ-t268RCpA",
+  },
 };
